e18e311f1fcb54ec57061fea7bedec98a23ba87d
James Shaw <jamesshaw95@googlemail.com>
2017-08-28 17:50:31 +0100

    Added project source files to public git repo.

---
+++ Word Count/CountHelper.swift
@@ -0,0 +1,48 @@
+//
+//  CountHelper.swift
+//  Word Count
+//
+//  Created by James Shaw on 06/09/2016.
+//  Copyright © 2016 James Shaw. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class CountHelper: NSObject {
+    
+    // Static instance of the object
+    static let sharedCountHelper = CountHelper()
+    
+    func getNumberOfChars(_ text: String) -> Int {
+        return text.characters.count
+    }
+    
+    func getNumberOfCharsWithoutSpaces(_ text: String) -> Int {
+        return 0
+    }
+    
+    func getNumberOfWords(_ text: String) -> Int {
+        let wordArray = text.components(separatedBy: CharacterSet.punctuationCharacters).joined(separator: "").components(separatedBy: " ").filter{$0 != ""}
+        return wordArray.count
+    }
+    
+    func getNumberOfLines(_ text: String, textView: UITextView) -> Int {
+        let lines = Int(textView.contentSize.height / textView.font!.lineHeight)
+        return lines
+    }
+    
+    func getNumberOfParagraphs(_ text: String) -> Int {
+        var paragraphsArray = text.components(separatedBy: "\n")
+        
+        // Remove empty strings
+        while paragraphsArray.contains("") {
+            if let removeIndex = paragraphsArray.index(of: "") {
+                paragraphsArray.remove(at: removeIndex)
+            }
+        }
+        //---------
+        
+        return paragraphsArray.count
+    }
+}
---
+++ Word Count/CountSummaryView.swift
@@ -0,0 +1,119 @@
+//
+//  CountSummaryView.swift
+//  Word Count
+//
+//  Created by James Shaw on 08/09/2016.
+//  Copyright © 2016 James Shaw. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class CountSummaryView: UIView {
+    
+    // Labels
+    var charsLabel: UILabel!
+    var charsValueLabel: UILabel!
+    var wordsLabel: UILabel!
+    var wordsValueLabel: UILabel!
+    //------------
+    
+    // Seperator View
+    var seperatorView: UIView!
+    
+    // Margins
+    let xMargin: CGFloat = 20.0
+    let yMargin: CGFloat = 10.0
+    let labelHeight: CGFloat = 20.0
+    
+    init() {
+        super.init(frame: CGRect.zero)
+        
+        // Clear background
+        self.backgroundColor = UIColor.clear
+        
+        // Create seperatorView
+        self.seperatorView = UIView(frame: CGRect.zero)
+        self.seperatorView.backgroundColor = UIColor.white
+        self.addSubview(self.seperatorView)
+        //----------
+        
+        // Create charsLabel
+        self.charsLabel = UILabel(frame: CGRect.zero)
+        self.charsLabel.textColor = UIColor.white
+        self.charsLabel.textAlignment = .left
+        self.charsLabel.backgroundColor = UIColor.clear
+        self.charsLabel.text = "Characters:"
+        self.charsLabel.font = UIFont.systemFont(ofSize: 16.0, weight: UIFontWeightThin)
+        self.addSubview(self.charsLabel)
+        //--------------
+        
+        // Create charsValueLabel
+        self.charsValueLabel = UILabel(frame: CGRect.zero)
+        self.charsValueLabel.textColor = UIColor.white
+        self.charsValueLabel.textAlignment = .right
+        self.charsValueLabel.backgroundColor = UIColor.clear
+        self.charsValueLabel.text = "0"
+        self.charsValueLabel.font = UIFont.systemFont(ofSize: 16.0, weight: UIFontWeightThin)
+        self.addSubview(self.charsValueLabel)
+        //--------------
+        
+        // Create wordsLabel
+        self.wordsLabel = UILabel(frame: CGRect.zero)
+        self.wordsLabel.textColor = UIColor.white
+        self.wordsLabel.textAlignment = .left
+        self.wordsLabel.backgroundColor = UIColor.clear
+        self.wordsLabel.text = "Words:"
+        self.wordsLabel.font = UIFont.systemFont(ofSize: 16.0, weight: UIFontWeightThin)
+        self.addSubview(self.wordsLabel)
+        //--------------
+        
+        // Create wordsValueLabel
+        self.wordsValueLabel = UILabel(frame: CGRect.zero)
+        self.wordsValueLabel.textColor = UIColor.white
+        self.wordsValueLabel.textAlignment = .right
+        self.wordsValueLabel.backgroundColor = UIColor.clear
+        self.wordsValueLabel.text = "0"
+        self.wordsValueLabel.font = UIFont.systemFont(ofSize: 16.0, weight: UIFontWeightThin)
+        self.addSubview(self.wordsValueLabel)
+        //--------------
+    }
+    
+    // Called once we have a frame for the view
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        // Get the view's frame
+        let frame = self.frame
+        
+        // Separator View
+        let seperatorYMargin: CGFloat = 6.0
+        self.seperatorView.frame = CGRect(x: (frame.width / 2), y: seperatorYMargin, width: 0.5, height: frame.height - (2 * seperatorYMargin))
+        //----------
+        
+        // Width of value labels
+        let valueLabelWidth: CGFloat = 50.0
+        let valueLabelXMargin: CGFloat = 8.0
+        
+        // Set frame of charsLabel and charsValueLabel
+        let charsLabelWidth = (frame.width / 2) - self.xMargin - valueLabelWidth - valueLabelXMargin
+        self.charsLabel.frame = CGRect(x: self.xMargin, y: self.yMargin, width: charsLabelWidth, height: self.labelHeight)
+        let charsValueLabelX = (frame.width / 2) - self.xMargin - valueLabelWidth
+        self.charsValueLabel.frame = CGRect(x: charsValueLabelX, y: self.yMargin, width: valueLabelWidth, height: self.labelHeight)
+        //-----------
+        
+        // Set frame of wordsLabel
+        let wordsLabelX = (frame.width / 2) + self.xMargin
+        let wordsLabelWidth = (frame.width / 2) - self.xMargin - valueLabelWidth - valueLabelXMargin
+        self.wordsLabel.frame = CGRect(x: wordsLabelX, y: self.yMargin, width: wordsLabelWidth, height: self.labelHeight)
+        let wordsValueLabelX = frame.width - self.xMargin - valueLabelWidth
+        self.wordsValueLabel.frame = CGRect(x: wordsValueLabelX, y: self.yMargin, width: valueLabelWidth, height: self.labelHeight)
+        //-----------
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+    }
+
+}
